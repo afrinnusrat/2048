abfc4632fb8d3b8e344ab212f98602abc378fd07
thePmSquare <impmmmm@gmail.com>
2020-10-06 04:59:08 +0530

    Updates

---
+++ precache-manifest.5ba2e6701d963239ff4920e6f7e806b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba0e466abd87abbf45601a5cf4b91d3",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "384aaba5a1ce11d00384",
+    "url": "/2048/static/css/main.ed82e411.chunk.css"
+  },
+  {
+    "revision": "ce8b9baad5281945c588",
+    "url": "/2048/static/js/2.f6e91924.chunk.js"
+  },
+  {
+    "revision": "9f44a06d2b53862651f6d5feee4d3d80",
+    "url": "/2048/static/js/2.f6e91924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384aaba5a1ce11d00384",
+    "url": "/2048/static/js/main.6207b403.chunk.js"
+  },
+  {
+    "revision": "c3297bc798c8c34112c4",
+    "url": "/2048/static/js/runtime-main.557b35d6.js"
+  }
+]);
